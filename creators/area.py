1c30f672202dbf977dd8e96593dd5e132cbf831d
Michael Kazman <michael.kazman@gmail.com>
2021-11-23 00:57:58 -0500

    added placeholder code for graph creators

---
+++ creators/area.py
@@ -0,0 +1,8 @@
+def create_bokeh_graph():
+    return {}
+
+def create_altair_graph():
+    return {}
+
+def create_plotnine_graph():
+    return {}
---
+++ creators/utils.py
@@ -0,0 +1,28 @@
+def export_bokeh_graph(graph, path):
+    return None
+
+def export_altair_graph(graph, path):
+    return None
+
+def export_plotnine_graph(graph, path):
+    return None
+
+def generate_file_path(library, graph, id):
+    return 'TODO'
+
+# def set_bokeh_theme(graph, theme):
+#     # set theme
+#     theme = pick_random_theme()
+#     curdoc().theme = theme
+#     # generate graph
+#     create_bokeh_graph[graph](data)
+
+# def set_altair_theme(graph, theme):
+    
+
+# def set_plotnine_theme(graph, theme):
+#     # set theme
+#     theme = pick_random_theme()
+#     theme_set(theme)
+#     # generate graph
+#     create_plotnine_graph[graph](data)
