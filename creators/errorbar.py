6f51fc3a92a0ab952a879d65d9781013ea6b3277
SeniorFluffie <michael.kazman@gmail.com>
2021-11-29 04:26:12 -0500

    added support for error bars

--- creators/errorbar.py
+++ creators/errorbar.py
@@ -1,5 +1,39 @@
+from creators.utils import unpack_graph_object
+from bokeh.plotting import figure
+
+parameters = {
+    'width':    400,
+    'height':   400,
+    'colours': ['red', 'blue', 'green', 'orange', 'purple'],
+}
+
 def create_bokeh_graph(graph_object):
-    return {}
+    # format data
+    (X, y, y_error), style = unpack_graph_object(graph_object)
+    plot = figure(width=parameters['width'], height=parameters['height'])
+
+    # plot each layer
+    for index, y in enumerate(y):
+        colour = parameters['colours'][index]
+        yerrs = y_error[index]
+        plot = error_bar_bokeh(plot, X, y, yerrs, colour)
+        
+    return plot
+
+def error_bar_bokeh(fig, x, y, yerr, color):
+    # create line graph
+    fig.circle(x, y, color=color)
+    fig.line(x, y, line_color = color)
+
+    # add error bars (as applicable)
+    if yerr[0] != None:
+        y_err_x, y_err_y = [], []
+        for px, py, err in zip(x, y, yerr):
+            y_err_x.append((px, px))
+            y_err_y.append((py - err, py + err))
+        fig.multi_line(y_err_x, y_err_y, color=color)
+    
+    return fig
 
 def create_altair_graph(graph_object):
     return {}
--- generators/errorbar.py
+++ generators/errorbar.py
@@ -1,2 +1,30 @@
+import numpy as np
+from generators.utils import generate_random_distribution, generate_midpoint_displacement
+
+parameters = {
+    'y_start_range':  (50, 400),
+    'y_end_range':    (50, 400),
+    'x_start_range':  (0, 1),
+    'x_end_range':  (400, 1000),
+    'rough_range':  (0.7, 1.5),
+    'vertical_displacement_range':  (50, 150),
+    'num_iterations_range': (2, 4),
+    'num_samples_range': (4, 15),
+    'layers_range':  (1, 4),
+    'error_bar_range': (5, 20),
+}
+
 def generate_data():
-    return []
+    # pick a random distribution type
+    data_distributions = {
+        'midpoint': generate_midpoint_displacement,
+        'random': generate_random_distribution
+    }
+    distribution = np.random.choice(list(data_distributions.keys()))
+
+    # generate data
+    X, y = data_distributions[distribution](parameters)
+
+    # generate error values for each datapoint
+    y_error = [np.random.uniform(*parameters['error_bar_range'], X.shape) for _ in range(len(y))]
+    return (X, y, y_error)
