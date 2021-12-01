1cf86bd905bba1feb8474980f3bee162102d9c2f
SeniorFluffie <michael.kazman@gmail.com>
2021-12-01 01:24:29 -0500

    added support for altair violin plot

---
+++ generators/violin.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+parameters = {
+    'num_violins_range': (2, 7),
+    'num_samples_range': (60, 120),
+    'y_data_range': (40, 120),
+}
+
+def generate_data():
+    # generate random data
+    num_violins = np.random.randint(*parameters['num_violins_range'])
+    num_samples = np.random.randint(*parameters['num_samples_range'])
+
+    # generate names for violins ('A', 'B', etc.)
+    groups= [chr(65 + i) for i in range (0, num_violins)] 
+
+    # select random normal values
+    X_data = [np.random.choice(groups) for _ in range(num_samples)]
+    y_data = [np.random.normal(*parameters['y_data_range']) for _ in range(num_samples)]
+
+    # add 
+    X = pd.Series(X_data)
+    y = pd.Series(y_data)
+
+    return (X, y)
