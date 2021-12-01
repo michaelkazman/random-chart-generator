1c2a4659cc27e22ab69524206334b5d5e979ffb0
Michael Kazman <michael.kazman@gmail.com>
2021-12-01 00:52:16 -0500

    added altair support for errorbar (and fixed lineplot gen)

--- creators/line.py
+++ creators/line.py
@@ -7,7 +7,6 @@ import numpy as np
 parameters = {
     'width':    400,
     'height':   400,
-    'colours': ['red', 'blue', 'green', 'orange', 'purple'],
     'matplot_marker': 'o',
     'bokeh_marker': "dot",
     'marker_size': 25,
@@ -20,43 +19,27 @@ def create_bokeh_graph(graph_object):
     
     # draw each line individually
     for index, y_list in enumerate(y):
-        colour = parameters['colours'][index]
-        p.line(X, y_list, line_color = colour)
-        getattr(p, parameters['bokeh_marker'])(X, y_list, line_color = colour, size=parameters['marker_size'])
+        p.line(X, y_list)
+        getattr(p, parameters['bokeh_marker'])(X, y_list, size=parameters['marker_size'])
     
     return p
 
 def create_altair_graph(graph_object):
     # unpack data
-    (X, y_lines), style = unpack_graph_object(graph_object)
-    num_lines = len(y_lines)
+    (X, y), style = unpack_graph_object(graph_object)
 
-    # create labels to group lines by
-    line_names = np.copy(y_lines)
-    for i in range(num_lines):
-        line_names[i, :] = str(i)
-
-    # format data to be appropriate for a data frame
-    X = np.append(X, [X] * (num_lines - 1))
-    y = y_lines.flatten()
-    line_names = line_names.flatten()
+    # for each layer, make data frame and store its line graph
+    layered_lines = []
+    for i in range(0, len(y)):
+        source = pd.DataFrame({"x": X, "y": y[i]})
+        lines = alt.Chart(source).mark_line().encode(
+            x='x',
+            y='y',
+        )
+        layered_lines.append(lines)
     
-    # create data frame
-    source = pd.DataFrame({
-        'x': X,
-        'y': y,
-        'line_names': line_names
-    })
-
-    # create line graph
-    chart = alt.Chart(source).mark_line().encode(
-        x = 'x',
-        y = 'y',
-        color = 'line_names',
-    ).properties(
-        width=parameters['width'],
-        height=parameters['height'],
-    )
+    # make final chart by layering
+    chart = alt.layer(*layered_lines)
     return chart
 
 def create_plotnine_graph(graph_object):
