966e37623d10e1cba80a40d370f145e77a47a610
Michael Kazman <michael.kazman@gmail.com>
2021-12-02 02:03:04 -0500

    added noise to bar chart generation

--- generators/bar.py
+++ generators/bar.py
@@ -3,15 +3,16 @@ import numpy as np
 parameters = {
     'x_range': (-5, 5),
     'height_range': (0, 50),
-    'features_range': (4, 5 * 2) # the 2nd argument of features_range should be 2 * x_range
+    'features_range': (4, 5 * 2), # the 2nd argument of features_range should be 2 * x_range
+    'noise_range': (0, 0.15),
 }
 
 def generate_data():
     # get features
     features = np.random.randint(*parameters['features_range'])
 
-    # generate x and y
-    X = np.linspace(*parameters['x_range'], features)
+    # generate x (rounded to two decimal places) and y
+    X = np.around(np.linspace(*parameters['x_range'], features), decimals=2)
     y = [np.random.uniform(*parameters['height_range']) for _ in range (0, features)]
     
     # random chance of correlation
@@ -22,6 +23,12 @@ def generate_data():
         if (correlation == "negative"):
             # reverse sorted array
             y = y[::-1]
+        
+        # random noise level
+        noise = np.random.uniform(*parameters['noise_range'], len(y))
+        for i in range (0, len(y)):
+            operation = np.random.choice([-1, 1])
+            y[i] += operation * (y[i] * noise[i])
     
     # random chance of graph being vertical/horizontal bar chart
     is_vertical = bool(np.random.randint(2))
