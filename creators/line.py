d3b21b6f409458c602f1d0b215747b381a419a6b
Michael Kazman <michael.kazman@gmail.com>
2021-12-03 03:15:28 -0500

    added plotnine support for line graph

--- creators/line.py
+++ creators/line.py
@@ -3,6 +3,7 @@ from creators.utils import unpack_graph_object
 import altair as alt
 import pandas as pd
 import numpy as np
+import plotnine as p9
 
 parameters = {
     'width':    400,
@@ -43,6 +44,31 @@ def create_altair_graph(graph_object):
     return p
 
 def create_plotnine_graph(graph_object):
-    # unpack data
-    (X, y), style = unpack_graph_object(graph_object)
-    return {}
+     # unpack data
+    (X, y_layers), style = unpack_graph_object(graph_object)
+    num_layers = len(y_layers)
+    
+    # create labels to group layers by
+    layer_names = np.copy(y_layers)
+    for i in range(num_layers):
+        layer_names[i, :] = i
+
+    # format data to be appropriate for a data frame
+    X = np.append(X, [X] * (num_layers - 1))
+    y_layers = y_layers.flatten()
+    layer_names = layer_names.flatten()
+    layer_names = [chr(int(i)+65) for i in layer_names]
+
+    # create data frame
+    data = pd.DataFrame({
+        'X': X,
+        'y': y_layers,
+        'layer_names': layer_names
+    })
+    
+    # make plot for each layer
+    p = p9.ggplot(data=data, mapping=p9.aes(x=X, y=y_layers, color=layer_names)) + p9.geom_line()
+
+
+
+    return p
