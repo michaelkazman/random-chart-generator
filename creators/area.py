ce58a6701bf626cdd9e30d1a2602221ab6d71a62
Michael Kazman <michael.kazman@gmail.com>
2021-12-03 02:27:09 -0500

    added plotnine support for area plot

--- creators/area.py
+++ creators/area.py
@@ -1,5 +1,6 @@
 import numpy as np
 import altair as alt
+import plotnine as p9
 import pandas as pd
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
@@ -8,7 +9,7 @@ from creators.utils import unpack_graph_object
 parameters = {
     'width':    400,
     'height':   400,
-    'colours': ['red', 'blue', 'green', 'orange', 'purple']
+    'colors': ['red', 'blue', 'green', 'orange', 'purple']
 }
 
 def create_bokeh_graph(graph_object):
@@ -22,15 +23,15 @@ def create_bokeh_graph(graph_object):
     layers = { 'x': X }
     layers.update(dict([(str(index), layer) for index, layer in enumerate(y)]))
 
-    # ensure each layer has a different colour
-    layer_colours = parameters['colours'][:num_layers]
+    # ensure each layer has a different color
+    layer_colors = parameters['colors'][:num_layers]
     
     # create source
     df = ColumnDataSource(data=layers)
     
     # make figure
     p = figure(width=parameters['width'], height=parameters['height'])
-    p.varea_stack(layer_names, x='x', source=df, color=layer_colours)
+    p.varea_stack(layer_names, x='x', source=df, color=layer_colors)
 
     return p
     
@@ -68,4 +69,29 @@ def create_altair_graph(graph_object):
     return p
 
 def create_plotnine_graph(graph_object):
-    return {}
+    # unpack data
+    (X, y_layers), style = unpack_graph_object(graph_object)
+    num_layers = len(y_layers)
+    
+    # create labels to group layers by
+    layer_names = np.copy(y_layers)
+    for i in range(num_layers):
+        layer_names[i, :] = i
+
+    # format data to be appropriate for a data frame
+    X = np.append(X, [X] * (num_layers - 1))
+    y_layers = y_layers.flatten()
+    layer_names = layer_names.flatten()
+
+    layer_names = [chr(int(i)+65) for i in layer_names]
+
+    data = pd.DataFrame({
+        'X': X,
+        'y': y_layers,
+        'layer_names': layer_names
+    })
+    
+    # make plot for each layer
+    p = p9.ggplot(data=data, mapping=p9.aes(x=X, y=y_layers, fill=layer_names)) + p9.geom_area()
+    
+    return p
