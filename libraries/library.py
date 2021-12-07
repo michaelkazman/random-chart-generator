056bec5c35623dd4af4b99a89f263cccd7f3661b
Michael Kazman <michael.kazman@gmail.com>
2021-12-07 03:07:19 -0500

    added library files (and base class)

---
+++ libraries/library.py
@@ -0,0 +1,28 @@
+class Library():
+    # occurs before any generation is run
+    # used as a setup / initialization step
+    def setup_hook():
+        pass
+
+    # occurs before the creation is run
+    # typically includes setup like setting the theme, etc.
+    def pre_creation_hook():
+        pass
+    
+    # retrieves the theme from the corresponding directory
+    def get_theme():
+        raise NotImplementedError
+
+    # sets the theme 
+    def set_theme(theme):
+        raise NotImplementedError
+
+    # occurs after the creation is run, passes in the graph
+    # in case any modifications are needed
+    def post_creation_hook(graph):
+        pass
+
+    # exports the provided graph to the specified file path
+    def export_graph(graph, file_path):
+        raise NotImplementedError
+
