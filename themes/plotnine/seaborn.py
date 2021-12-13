239024f1fb9f22651f7aecd0cca58b11661e89e1
Michael Kazman <michael.kazman@gmail.com>
2021-12-13 04:23:13 -0500

    added themes for various libraries, updated a few existing

---
+++ themes/plotnine/seaborn.py
@@ -0,0 +1,34 @@
+import plotnine as p9
+
+class theme_seaborn(p9.themes.theme):
+    """
+    Theme for seaborn.
+
+    Credit to Michael Waskom's seaborn:
+
+        - http://stanford.edu/~mwaskom/software/seaborn
+        - https://github.com/mwaskom/seaborn
+
+    Parameters
+    ----------
+    style: str in ``['whitegrid', 'darkgrid', 'nogrid', 'ticks']``
+        Style of axis background.
+    context: str in ``['notebook', 'talk', 'paper', 'poster']``
+        Intended context for resulting figures.
+    font : str
+        Font family, see matplotlib font manager.
+    font_scale : float, optional
+        Separate scaling factor to independently scale the
+        size of the font elements.
+    """
+
+    def __init__(self, style='darkgrid', context='notebook', font='sans-serif', font_scale=1):
+        p9.themes.theme.__init__(self,
+                       aspect_ratio=p9.options.get_option('aspect_ratio'),
+                       dpi=p9.options.get_option('dpi'),
+                       figure_size=p9.options.get_option('figure_size'),
+                       panel_spacing=0.1,
+                       complete=True)
+        d = p9.themes.seaborn_rcmod.set(context=context, style=style,
+                        font=font, font_scale=font_scale)
+        self._rcParams.update(d)
---
+++ themes/plotnine/tufte.py
@@ -0,0 +1,48 @@
+import plotnine as p9
+
+class theme_tufte(p9.themes.theme_bw):
+    """
+    Tufte Maximal Data, Minimal Ink Theme
+    Theme based on Chapter 6 'Data-Ink Maximization and Graphical
+    Design of Edward Tufte *The Visual Display of Quantitative
+    Information*. No border, no axis lines, no grids. This theme
+    works best in combination with :class:`geom_rug()` or
+    :class:`geom_rangeframe()`.
+    The default font family is set to 'serif' as he uses serif
+    fonts for labels in 'The Visual Display of Quantitative
+    Information'. The serif font used by Tufte in his books is
+    a variant of Bembo, while the sans serif font is Gill Sans.
+    If these fonts are installed on your system, consider setting
+    them explicitly via the argument `base_family`.
+    Parameters
+    ----------
+    base_size : int, optional
+        Base font size. All text sizes are a scaled versions of
+        the base font size. Default is 11.
+    base_family : str, optional
+        Base font family.
+    ticks: logical, optional
+        Show axis ticks?
+    Tufte, Edward R. (2001) The Visual Display of Quantitative
+    Information, Chapter 6.
+    Translated from the R ggthemes package by hyiltiz <hyiltiz@gmail.com>.
+    Released under GNU GPL v2 license or later.
+    """
+
+    def __init__(self, base_size=11, base_family=None, ticks=True):
+        p9.themes.theme_bw.__init__(self, base_size, base_family)
+        self.add_theme(p9.themes.theme(
+            legend_background=p9.themes.elements.element_blank(),
+            legend_key=p9.themes.elements.element_blank(),
+            panel_background=p9.themes.elements.element_blank(),
+            panel_border=p9.themes.elements.element_blank(),
+            strip_background=p9.themes.elements.element_blank(),
+            plot_background=p9.themes.elements.element_blank(),
+            axis_line=p9.themes.elements.element_blank(),
+            panel_grid=p9.themes.elements.element_blank()
+        ), inplace=True)
+
+        if not ticks:
+            self.add_theme(p9.themes.theme(
+                axis_ticks=p9.themes.elements.element_blank()
+                ), inplace=True)
