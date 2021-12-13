41cb7ffecd7e53a5135a6cddd648610077a4e65b
Michael Kazman <michael.kazman@gmail.com>
2021-12-13 01:51:12 -0500

    fixed contour generation and added bokeh and plotnine support

--- creators/contour.py
+++ creators/contour.py
@@ -6,98 +6,113 @@ import matplotlib.pyplot as plt
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 from utils.creators import unpack_graph_object
+import math
 
 parameters = {
     'width': 400,
-    'height': 400
+    'height': 400,
 }
 
 def create_bokeh_graph(graph_object):
-    # format data
+  # format data
     (X, y, z), style = unpack_graph_object(graph_object)
-    df = get_contour(X, y, z)
+    df = get_contour_data(X, y, z)
 
     # initialize figure
     p = figure(
-        plot_width=parameters.get('width'),
-        plot_height=parameters.get('height'),
+        plot_width=500,
+        plot_height=300,
         x_range=[X[0][0], X[-1][-1]],
         y_range=[y[0][0], y[-1][-1]],
         x_axis_label='X',
         y_axis_label='y',
         toolbar_location=None,
     )
+    
     # plot various lines (representing the contour segments)
     p.multi_line(
-        xs='xs',
-        ys='ys',
+        xs='X',
+        ys='y',
         line_color='line_color',
         source=df
     )
 
-    return p
-
-def get_contour(X, y, z):
-    # format data
-    plt.ioff()
-    cs = plt.contour(X, y, z)
-    xs, ys, col = [], [], []
-    isolevelid = 0
-    
-    # go through each contour level
-    for isolevel in cs.collections:
-        isocol = isolevel.get_color()[0]
-        thecol = 3 * [None]
-        isolevelid += 1
-
-        # generate colour values
-        for i in range(3):
-            thecol[i] = int(255 * isocol[i])
-        thecol = '#%02x%02x%02x' % (thecol[0], thecol[1], thecol[2])
-
-        # map out contour paths
-        for path in isolevel.get_paths():
-            v = path.vertices
-            x = v[:, 0]
-            y = v[:, 1]
-            xs.append(x.tolist())
-            ys.append(y.tolist())
-            col.append(thecol)
-
-    df = ColumnDataSource({
-            'xs': xs,
-            'ys': ys,
-            'line_color': col
-        }
+    # plot contour values
+    p.text(
+        x='x_text',
+        y='y_text',
+        text='z',
+        text_color='line_color',
+        text_baseline='middle',
+        text_align='center',
+        source=df,
     )
 
-    return df
+    return p
 
 def create_altair_graph(graph_object):
+    # not supported by vega-lite (the altair renderer)
     return {}
 
 def create_plotnine_graph(graph_object):
-    # unpack data
+    # unpack / format data
     (X, y, z), style = unpack_graph_object(graph_object)
+    data = get_contour_data(X, y, z)
 
-    # format data for data frame
-    X = X.flatten()
-    y = y.flatten()
+    # flatten arrays
+    X_flattened = np.hstack(data['X']).flatten()
+    y_flattened = np.hstack(data['y']).flatten()
+    z_flattened = np.hstack([[data['z'][i]] * len(row) for i, row in enumerate(data['X'])]).flatten()
 
-    # create data frame
+    # create df
     df = pd.DataFrame({
-        'X': X,
-        'y': y,
+        'X': X_flattened,
+        'y': y_flattened,
+        'z': z_flattened,
     })
 
-    # create plot
-    p = p9.ggplot(p9.aes(
-        x='X',
-        y='y',
-    ), data=df) + p9.stat_density_2d(
-        p9.aes(fill='..level..'),
-        levels=np.array([.05, 0.1, 0.15, 0.2])*0.1,
-        geom='polygon',
-    ) + p9.lims(x=(0.5, 6), y =(40, 110))
+    p = (
+        p9.ggplot(p9.aes(x='X', y='y'), data=df)
+        + p9.stat_density_2d(
+        p9.aes(),
+        )
+        + p9.scales.scale_x_continuous(limits=(-2, 2), expand=(0, 0))
+        + p9.scales.scale_y_continuous(limits=(-2, 2), expand=(0, 0))
+        + p9.geom_text(p9.aes(x='X', y='y', label='z'), data=df[::200])
+    )
 
     return p
+
+def get_contour_data(X, Y, Z):
+    # generate matplotlib contour
+    cs = plt.contour(X, Y, Z)
+    X_points, y_points, z_points, z_flattened = [], [], np.array([]), np.array([])
+    x_text, y_text, line_color = np.array([]), np.array([]), np.array([])
+
+    # go through each contour level
+    for i, level in enumerate(cs.collections):
+        level_color = level.get_color()[0]
+        level_rgb_color = [int(255 * rgb_value) for rgb_value in level_color]
+        level_hex_color = "#{0:02x}{1:02x}{2:02x}".format(*level_rgb_color)
+        z = round(cs.get_array()[i], 2)
+
+        # append points, text coordinates, text, and colour
+        for path in level.get_paths():
+            x, y = path.vertices[:, 0], path.vertices[:, 1]
+            X_points.append(x.tolist())
+            y_points.append(y.tolist())
+            x_text = np.append(x_text, x[math.floor(len(x) / 2)])
+            y_text = np.append(y_text, y[math.floor(len(y) / 2)])
+            z_points = np.append(z_points, z)
+            line_color = np.append(line_color, level_hex_color)
+
+    # create dict based on scraped data
+    df = {
+      'X': np.array(X_points, dtype=object),
+      'y': np.array(y_points, dtype=object),
+      'z': z_points,
+      'x_text': x_text,
+      'y_text':y_text,
+      'line_color': line_color
+    }
+    return df
