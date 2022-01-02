d5bc09ba314d3cd7d374d403d9b26f4653a240f0
SeniorFluffie <michael.kazman@gmail.com>
2022-01-02 02:39:33 -0500

    updated contour styles, moved contour generation to be in generation file

--- creators/contour.py
+++ creators/contour.py
@@ -1,29 +1,28 @@
 import numpy as np
 import pandas as pd
 import plotnine as p9
-import matplotlib.pyplot as plt
-
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource
 from utils.creators import unpack_graph_object
-import math
-
-parameters = {
-    'width': 400,
-    'height': 400,
-}
 
 def create_bokeh_graph(graph_object):
   # format data
-    (X, y, z), styles = unpack_graph_object(graph_object)
-    df = get_contour_data(X, y, z)
+    (X, y, z, x_text, y_text, X_range, y_range), styles = unpack_graph_object(graph_object)
+    df = {
+        'X': X,
+        'y': y,
+        'z': z,
+        'x_text': x_text,
+        'y_text': y_text,
+        'color': styles.get('color') if styles.get('use_random_colors') else styles.get('color')[:1] * len(styles.get('color')),
+        'text_color': [styles.get('text_color')] * len(styles.get('color'))
+    }
 
     # initialize figure
     p = figure(
         plot_width=500,
         plot_height=300,
-        x_range=[X[0][0], X[-1][-1]],
-        y_range=[y[0][0], y[-1][-1]],
+        x_range=X_range,
+        y_range=y_range,
         x_axis_label='X',
         y_axis_label='y',
         toolbar_location=None,
@@ -33,7 +32,7 @@ def create_bokeh_graph(graph_object):
     p.multi_line(
         xs='X',
         ys='y',
-        line_color='line_color',
+        line_color='color',
         line_width=styles.get('line_thickness'),
         source=df
     )
@@ -43,7 +42,7 @@ def create_bokeh_graph(graph_object):
         x='x_text',
         y='y_text',
         text='z',
-        text_color='line_color',
+        text_color='text_color',
         text_baseline='middle',
         text_align='center',
         source=df,
@@ -57,13 +56,12 @@ def create_altair_graph(graph_object):
 
 def create_plotnine_graph(graph_object):
     # unpack / format data
-    (X, y, z), styles = unpack_graph_object(graph_object)
-    data = get_contour_data(X, y, z)
+    (X, y, z, *_), styles = unpack_graph_object(graph_object)
 
     # flatten arrays
-    X_flattened = np.hstack(data['X']).flatten()
-    y_flattened = np.hstack(data['y']).flatten()
-    z_flattened = np.hstack([[data['z'][i]] * len(row) for i, row in enumerate(data['X'])]).flatten()
+    X_flattened = np.hstack(X).flatten()
+    y_flattened = np.hstack(y).flatten()
+    z_flattened = np.hstack([[z[i]] * len(row) for i, row in enumerate(X)]).flatten()
 
     # create df
     df = pd.DataFrame({
@@ -74,56 +72,11 @@ def create_plotnine_graph(graph_object):
 
     p = (
         p9.ggplot(p9.aes(x='X', y='y'), data=df)
-        + p9.stat_density_2d(
-        p9.aes(),
-        )
+        + p9.stat_density_2d(geom='polygon', show_legend=False, color=styles.get('color', [])[0], fill=None, size=styles.get('line_thickness'))
         + p9.scales.scale_x_continuous(limits=(-2, 2), expand=(0, 0))
         + p9.scales.scale_y_continuous(limits=(-2, 2), expand=(0, 0))
-        + p9.geom_text(p9.aes(x='X', y='y', label='z'), data=df[::200])
+        + p9.geom_text(p9.aes(x='X', y='y', label='z', group='X'), data=df[::200])
+        + p9.theme(figure_size=(styles.get('width'), styles.get('height')))
     )
 
     return p
-
-'''
-Title: get_contour_data()
-Author: BR123
-Date: 2016
-Code version: N/A
-Availability: https://docs.bokeh.org/en/latest/docs/gallery/histogram.html
-
-The contour function below was used to plot values with matplotlib and scrape the generated plot values
-'''
-
-def get_contour_data(X, Y, Z):
-    # generate matplotlib contour
-    cs = plt.contour(X, Y, Z)
-    X_points, y_points, z_points, z_flattened = [], [], np.array([]), np.array([])
-    x_text, y_text, line_color = np.array([]), np.array([]), np.array([])
-
-    # go through each contour level
-    for i, level in enumerate(cs.collections):
-        level_color = level.get_color()[0]
-        level_rgb_color = [int(255 * rgb_value) for rgb_value in level_color]
-        level_hex_color = "#{0:02x}{1:02x}{2:02x}".format(*level_rgb_color)
-        z = round(cs.get_array()[i], 2)
-
-        # append points, text coordinates, text, and colour
-        for path in level.get_paths():
-            x, y = path.vertices[:, 0], path.vertices[:, 1]
-            X_points.append(x.tolist())
-            y_points.append(y.tolist())
-            x_text = np.append(x_text, x[math.floor(len(x) / 2)])
-            y_text = np.append(y_text, y[math.floor(len(y) / 2)])
-            z_points = np.append(z_points, z)
-            line_color = np.append(line_color, level_hex_color)
-
-    # create dict based on scraped data
-    df = {
-      'X': np.array(X_points, dtype=object),
-      'y': np.array(y_points, dtype=object),
-      'z': z_points,
-      'x_text': x_text,
-      'y_text':y_text,
-      'line_color': line_color
-    }
-    return df
--- generators/contour.py
+++ generators/contour.py
@@ -1,6 +1,7 @@
-import numpy as np
+import math
 import numpy as np
 import scipy.stats as st
+import matplotlib.pyplot as plt
 
 parameters = {
     "num_points": 500,
@@ -35,8 +36,56 @@ def generate_data():
     z = np.reshape(kernel(positions).T, X.shape)
     z[z==0] = 0.01
 
+    # generate contour and get values
+    (contour_X, contour_y, contour_z, x_text, y_text) = get_contour_data(X, y, z).values()
+    
     return {
-        'X': X,
-        'y': y,
-        'z': z
+        'X': contour_X,
+        'y': contour_y,
+        'z': contour_z,
+        'x_text': x_text,
+        'y_text': y_text,
+        'X_range': (X[0][0], X[-1][-1]),
+        'y_range': (y[0][0], y[-1][-1]),
+        'num_repeats': len(contour_X),
     }
+
+'''
+Title: get_contour_data()
+Author: BR123
+Date: 2016
+Code version: N/A
+Availability: https://docs.bokeh.org/en/latest/docs/gallery/histogram.html
+
+The contour function below was used to plot values with matplotlib and scrape the generated plot values
+'''
+
+def get_contour_data(X, Y, Z):
+    # generate matplotlib contour
+    cs = plt.contour(X, Y, Z)
+    X_points, y_points, z_points = [], [], np.array([])
+    x_text, y_text = np.array([]), np.array([])
+
+    # go through each contour level
+    for i, level in enumerate(cs.collections):
+        z = round(cs.get_array()[i], 2)
+
+        # append points, text coordinates, text, and colour
+        for path in level.get_paths():
+            x, y = path.vertices[:, 0], path.vertices[:, 1]
+            X_points.append(x.tolist())
+            y_points.append(y.tolist())
+            x_text = np.append(x_text, x[math.floor(len(x) / 2)])
+            y_text = np.append(y_text, y[math.floor(len(y) / 2)])
+            z_points = np.append(z_points, z)
+
+    # create dict based on scraped data
+    df = {
+      'X': np.array(X_points, dtype=object),
+      'y': np.array(y_points, dtype=object),
+      'z': z_points,
+      'x_text': x_text,
+      'y_text': y_text,
+    }
+
+    return df
