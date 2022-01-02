5a065feaf6bca35ef556145996467c35eaeac864
Michael Kazman <michael.kazman@gmail.com>
2022-01-02 05:06:22 -0500

    added plotnine support for kd and fixed styles

--- creators/kd.py
+++ creators/kd.py
@@ -1,8 +1,87 @@
+import numpy as np
+import pandas as pd
+import plotnine as p9
+import altair as alt
+from utils.creators import unpack_graph_object
+from bokeh.plotting import figure
+from utils.creators import convert_numbers_to_letters, unpack_graph_object
+
 def create_bokeh_graph(graph_object):
-    return {}
+    (X, y, *_), styles = unpack_graph_object(graph_object)
+
+    p = figure(
+        toolbar_location=None,
+        width = styles.get('width'),
+        height = styles.get('height'),
+    )
+    for i in range(X.shape[0]):
+        p.line(X[i], y[i], line_color=styles.get('color')[i], line_width=4, alpha=0.7)
+
+    p.y_range.start = 0
+    p.xaxis.axis_label = 'x'
+    p.yaxis.axis_label = 'Pr(x)'
+    p.grid.grid_line_color="white"
+
+    return p
 
 def create_altair_graph(graph_object):
-    return {}
+    # unpack data
+    (X, y, *_), styles = unpack_graph_object(graph_object)
+
+    # for each layer, make data frame and store its line graph
+    layered_lines = []
+    for i in range(X.shape[0]):
+        df = pd.DataFrame({'x': X[i], 'y': y[i]})
+        lines = alt.Chart(df).mark_line().encode(
+            x='x',
+            y='y',
+            color = alt.value(styles.get('color')[i]),
+        ).properties(
+            width=styles.get('width'),
+            height=styles.get('height'),
+        )
+        
+        layered_lines.append(lines)
+    
+    # make final chart by layering
+    p = alt.layer(*layered_lines)
+    return p
 
 def create_plotnine_graph(graph_object):
-    return {}
+     # unpack data
+    (X, y, *_), styles = unpack_graph_object(graph_object)
+    num_layers = X.shape[0]
+    
+    # create labels to group layers by
+    layer_names = np.copy(y)
+    for i in range(num_layers):
+        layer_names[i, :] = i    
+
+    # format data to be appropriate for a data frame
+    X = X.flatten()
+    y = y.flatten()
+    layer_names = convert_numbers_to_letters(layer_names.flatten())
+
+    # create data frame
+    df = pd.DataFrame({
+        'X': X,
+        'y': y,
+    })
+    
+    # make plot for each layer
+    p = (
+        p9.ggplot(
+            data=df, 
+            mapping=p9.aes(
+                x='X', 
+                y='y',
+                color=layer_names,
+                fill=layer_names,
+            )
+        ) 
+        + p9.geom_line(show_legend='None') 
+        + p9.scale_color_manual(values=styles.get('color'))
+        + p9.scale_fill_manual(values=styles.get('color'))
+    )
+
+    return p
--- generators/kd.py
+++ generators/kd.py
@@ -1,2 +1,44 @@
+import numpy as np
+from scipy.stats import norm
+from sklearn.neighbors import KernelDensity
+
+parameters = {
+    'mean_range': [0, 5],
+    'sigma_range': [1, 1],
+    'size_range': (0.3, 0.7),
+    'N': 100,
+    'num_distribution_range': (1, 3),
+}
+
 def generate_data():
-    return []
+    # generate parameters
+    N = parameters.get('N')
+    num_distributions = np.random.randint(*parameters.get(('num_distribution_range')))
+    means = np.random.uniform(*parameters.get('mean_range'), num_distributions)
+    sigmas = np.random.uniform(*parameters.get('sigma_range'), num_distributions)
+    sizes = np.random.uniform(*parameters.get('size_range'), num_distributions)
+
+    X = np.concatenate(
+        [np.random.normal(means[i], sigmas[i], int(sizes[i] * N)) for i in range(num_distributions)]
+    )[:, np.newaxis]
+
+    X_plot = np.linspace(-5, 10, 1000)[:, np.newaxis]
+
+    true_dens = 0
+    kernel_options = ['gaussian', 'epanechnikov', 'exponential', 'linear', 'cosine']
+    kernels = np.random.choice(kernel_options, size=num_distributions, replace=False)
+
+    X_data, y_data = [], []
+    for i in range(num_distributions):
+        true_dens = sizes[i] * norm(means[i], sigmas[i]).pdf(X_plot[:, 0]) + true_dens
+        kde = KernelDensity(kernel=kernels[i], bandwidth=0.5).fit(X)
+        X_i = X_plot[:, 0]
+        y_i = np.exp(kde.score_samples(X_plot)) 
+        X_data.append(X_i)
+        y_data.append(y_i)
+    
+    return {
+        'X': np.array(X_data),
+        'y': np.array(y_data),
+        'num_repeats': len(X_data)
+    }
