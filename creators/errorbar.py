a5898442fe3821e13626a72af8952b01bb0e0348
SeniorFluffie <michael.kazman@gmail.com>
2022-01-02 05:06:56 -0500

    updated errorbar stylization

--- creators/errorbar.py
+++ creators/errorbar.py
@@ -2,22 +2,15 @@ import numpy as np
 import pandas as pd
 import altair as alt
 import plotnine as p9
-
 from bokeh.plotting import figure
 from utils.creators import convert_numbers_to_letters, unpack_graph_object
-
-parameters = {
-    'width':    400,
-    'height':   400,
-    'alt_point_size': 40,
-}
 
 def create_bokeh_graph(graph_object):
     # format data
     (X, y, y_errors, *_), styles = unpack_graph_object(graph_object)
     p = figure(
-        width=parameters.get('width'), 
-        height=parameters.get('height'), 
+        width=styles.get('width'), 
+        height=styles.get('height'), 
         x_axis_label='X',
         y_axis_label='y',
         toolbar_location=None,
@@ -26,15 +19,15 @@ def create_bokeh_graph(graph_object):
     # plot each layer
     for i, (y, y_error) in enumerate(zip(y, y_errors)):
         # plot points
-        p.circle(X, y, color=styles.get('color')[i], line_width=styles.get("line_thickness"))
-        p.line(X, y, color=styles.get('color')[i], line_width=styles.get("line_thickness"))
+        getattr(p, styles.get('marker_type'))(X, y, color=styles.get('color')[i], size=styles.get('marker_size'))
+        p.line(X, y, color=styles.get('color')[i], line_width=styles.get('line_thickness'))
 
         # plot vertical error lines
         y_err_x, y_err_y = [], []
         for px, py, err in zip(X, y, y_error):
             y_err_x.append((px, px))
             y_err_y.append((py - err, py + err))
-        p.multi_line(y_err_x, y_err_y, color=styles.get('color')[i], line_width=styles.get("line_thickness"))
+        p.multi_line(y_err_x, y_err_y, color=styles.get('color')[i], line_width=styles.get('error_bar_thickness'))
 
     return p
 
@@ -44,13 +37,15 @@ def create_altair_graph(graph_object):
     layered_points, layered_errorbars, layered_lines = [], [], []
 
     # create each 'line' layer
-    for y_i, y_error in zip(y, y_errors):
+    for i, (y_i, y_error) in enumerate(zip(y, y_errors)):
         # set up data frame
         df = pd.DataFrame({
             'X': X,
             'y': y_i,
-            'yerr': y_error}
-        )
+            'yerr': y_error,
+            'color': styles.get('color')[i],
+            'size': styles.get('marker_size')
+        })
 
         # the base chart for other charts to build upon
         base = alt.Chart(df).transform_calculate(
@@ -61,7 +56,7 @@ def create_altair_graph(graph_object):
         # create points
         points = base.mark_point(
             filled=True,
-            size=parameters.get('alt_point_size'),
+            size=alt.Value(styles.get('marker_size')),
         ).encode(
             x='X',
             y='y'
@@ -70,15 +65,17 @@ def create_altair_graph(graph_object):
         # create error bars
         errorbars = base.mark_errorbar().encode(
             x='X',
-            # y='ymin:Q',
             y=alt.Y('ymin:Q', axis=alt.Axis(title='y')),
-            y2='ymax:Q'
+            y2='ymax:Q',
+            strokeWidth=alt.value(styles.get('error_bar_thickness'))
         )
 
         # link the points and lines
         lines = base.mark_line().encode(
             x='X',
             y='y',
+            color=alt.Color('color', legend=None),
+            strokeWidth=alt.value(styles.get('line_thickness'))
         )
 
         # store that layers points, error bars, and lines
@@ -87,15 +84,13 @@ def create_altair_graph(graph_object):
         layered_lines.append(lines)
 
     # combine layers
-    p = alt.layer(*layered_points, *layered_errorbars, *layered_lines)
+    p = alt.layer(*layered_points, *layered_errorbars, *layered_lines).properties(width=styles.get('width'), height=styles.get('height'))
     return p
 
 def create_plotnine_graph(graph_object):
      # format data
     (X, y_layers, y_errors, *_), styles = unpack_graph_object(graph_object)
     num_layers = len(y_layers)
-
-    colors = np.array([[styles.get('color')[i]] * y_layers.shape[1] for i in range(y_layers.shape[0])]).flatten()
 
     # create labels to group layers by, and get max and min for error bars
     layer_names = np.copy(y_layers)
@@ -130,11 +125,16 @@ def create_plotnine_graph(graph_object):
                 y='y', 
                 ymax='y_err_max', 
                 ymin='y_err_min',
-            )
+                color=layer_names,
+                fill=layer_names,
+            ),
         )
-        + p9.geom_line(show_legend='None', mapping=p9.aes(fill=colors, color=colors))
-        + p9.geom_point(show_legend='None', mapping=p9.aes(fill=colors, color=colors))
-        + p9.geom_errorbar(show_legend='None', mapping=p9.aes(fill=colors, color=colors))
+        + p9.geom_line(show_legend='None', size=styles.get('line_thickness'))
+        + p9.geom_point(show_legend='None', size=styles.get('marker_size'))
+        + p9.geom_errorbar(show_legend='None', size=styles.get('error_bar_thickness'))
+        + p9.theme(figure_size=(styles.get('width'), styles.get('height')))
+        + p9.scale_color_manual(values=styles.get('color'))
+        + p9.scale_fill_manual(values=styles.get('color'))
     )
 
     return p
