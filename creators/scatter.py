2477845ef3b70ae7a3181686dd95925fc8d6aa45
SeniorFluffie <michael.kazman@gmail.com>
2022-01-02 06:23:34 -0500

    updated scatter plot stylization

--- creators/scatter.py
+++ creators/scatter.py
@@ -5,11 +5,6 @@ from bokeh.plotting import figure
 from utils.creators import unpack_graph_object
 from bokeh.models import ColumnDataSource, Scatter
 
-parameters = {
-    'width': 300,
-    'height': 300,
-}
-
 def create_bokeh_graph(graph_object):
     # format data
     (X, y, *_), styles = unpack_graph_object(graph_object)
@@ -18,8 +13,8 @@ def create_bokeh_graph(graph_object):
     # create plot
     p = figure(
         title=None,
-        width=parameters.get('width'),
-        height=parameters.get('height'),
+        width=styles.get('width'),
+        height=styles.get('height'),
         x_axis_label='X',
         y_axis_label='y',
         min_border=0,
@@ -33,9 +28,9 @@ def create_bokeh_graph(graph_object):
         size=styles.get('size'),
         marker=styles.get('type'),
         fill_alpha=styles.get('opacity'),
-        fill_color=styles.get('fill'),
-        line_color=styles.get('color'),
-        line_width=styles.get('stroke'),
+        fill_color=styles.get('color'),
+        line_color=styles.get('stroke_color'),
+        line_width=styles.get('stroke_width'),
     )
     p.add_glyph(df, glyph)
 
@@ -54,12 +49,14 @@ def create_altair_graph(graph_object):
         size=styles.get('size'),
         fill=styles.get('color'),
         opacity=styles.get('opacity'),
+        stroke=styles.get('stroke_color'),
+        strokeWidth=styles.get('stroke_width'),
     ).encode(
         x='X',
         y='y',
     ).properties(
-        width=parameters.get('width'),
-        height=parameters.get('height'),
+        width=styles.get('width'),
+        height=styles.get('height'),
     )
 
     return p
@@ -77,12 +74,13 @@ def create_plotnine_graph(graph_object):
         p9.ggplot(data=df, mapping=p9.aes(x='X', y='y'))
         + p9.geom_point(
             show_legend='None',
-            fill=styles.get('fill'),
-            color=styles.get('color'),
-            stroke=styles.get('stroke'),
+            fill=styles.get('color'),
+            alpha=styles.get('opacity'),
+            color=styles.get('stroke_color'),
+            stroke=styles.get('stroke_width'),
             size=styles.get('size'),
-            alpha=styles.get('opacity')
         )
+        + p9.theme(figure_size=(styles.get('width'), styles.get('height')))
     )
     
     return p
