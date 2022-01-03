be8ac48d2b599f93568db8e661710f410d57cf17
SeniorFluffie <michael.kazman@gmail.com>
2022-01-03 04:17:45 -0500

    added histogram stylization

--- creators/histogram.py
+++ creators/histogram.py
@@ -4,12 +4,6 @@ import plotnine as p9
 from bokeh.plotting import figure
 from utils.creators import unpack_graph_object
 
-parameters = {
-    'width': 400,
-    'height': 400,
-    'alpha': 0.5,
-}
-
 def create_bokeh_graph(graph_object):
     # unpack data
     (X, y, *_), styles = unpack_graph_object(graph_object)
@@ -17,9 +11,12 @@ def create_bokeh_graph(graph_object):
     # the x-coordinates of the edges
     left_edges = X[:-1]
     right_edges = X[1:]
+    color = [styles.get('color')] * len(y)
 
     # create plot
     p = figure(
+        width=styles.get('width'),
+        height=styles.get('height'),
         title=None,
         x_axis_label='X',
         y_axis_label='y',
@@ -30,7 +27,9 @@ def create_bokeh_graph(graph_object):
         bottom=0, 
         left=left_edges, 
         right=right_edges, 
-        alpha=parameters.get('alpha'),
+        color=color,
+        alpha=styles.get('opacity'),
+        line_width=styles.get('bar_width'),
     )
 
     return p
@@ -42,13 +41,20 @@ def create_altair_graph(graph_object):
     # format data
     df = pd.DataFrame({
         'X': X[:-1], # set edges to right
-        'y': y, 
+        'y': y,
     })
 
     # create histogram chart
     p = alt.Chart(df).mark_bar().encode(
         x='X',
         y='y',
+    ).properties(
+        width=styles.get('width'),
+        height=styles.get('height'),
+    ).configure_mark(
+        color=styles.get('color'),
+        opacity=styles.get('opacity'),
+        size=styles.get('bar_width'),
     )
 
     return p
@@ -65,6 +71,10 @@ def create_plotnine_graph(graph_object):
     })
 
     # create plot
-    p = p9.ggplot(df, p9.aes(x='X', y=p9.after_stat('y'))) + p9.geom_histogram(bins=X.shape[0])
+    p = (
+        p9.ggplot(df, p9.aes(x='X', y=p9.after_stat('y')))
+        + p9.geom_histogram(bins=X.shape[0], fill=styles.get('color'), size=styles.get('bar_width'), alpha=styles.get('opacity'))
+        + p9.theme(figure_size=(styles.get('width'), styles.get('height')))
+    )
 
     return p
--- generators/histogram.py
+++ generators/histogram.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 parameters = {
-    'size_range': (100, 1000),
+    'size_range': (300, 1000),
     'bins_range': (7, 100),
     'mu_range': (0, 4),
     'sigma_range': (0.01, 0.5),
@@ -24,8 +24,8 @@ def generate_data():
     distribution = np.random.choice(list(data_distributions.keys()))
 
     # generate parameters
-    size = np.random.randint(*parameters['size_range'])
-    bins = np.random.randint(*parameters['bins_range'])
+    size = np.random.randint(*parameters.get('size_range', ()))
+    bins = np.random.randint(*parameters.get('bins_range', ()))
     
     # get data distribution (and ensure no negative y values)
     y, X = data_distributions[distribution](size, bins)
@@ -49,40 +49,40 @@ The basis for the following generation functions was taken from the above
 
 # generates a normal distribution (with random mu, sigma, and size)
 def generate_random_normal_distribution(size, bins):
-    mu = np.random.uniform(*parameters['mu_range'])
-    sigma = np.random.uniform(*parameters['sigma_range'])
+    mu = np.random.uniform(*parameters.get('mu_range', ()))
+    sigma = np.random.uniform(*parameters.get('sigma_range', ()))
     measured = np.random.normal(mu, sigma, size)
     y, X = np.histogram(measured, density=False, bins=bins)
     return y, X
 
 # generates a logistic normal distribution (with random mu, sigma, and size)
 def generate_random_lognormal_distribution(size, bins):
-    mu = np.random.uniform(*parameters['mu_range'])
-    sigma = np.random.uniform(*parameters['sigma_range'])
+    mu = np.random.uniform(*parameters.get('mu_range', ()))
+    sigma = np.random.uniform(*parameters.get('sigma_range', ()))
     measured = np.random.lognormal(mu, sigma, size)
     y, X = np.histogram(measured, density=True, bins=bins)
     return y, X
 
 # generates a gamma distribution (with random k, theta, and size)
 def generate_random_gamma_distribution(size, bins):
-    k = np.random.uniform(*parameters['k_range'])
-    theta = np.random.uniform(*parameters['theta_range'])
+    k = np.random.uniform(*parameters.get('k_range', ()))
+    theta = np.random.uniform(*parameters.get('theta_range', ()))
     measured = np.random.gamma(k, theta, size)
     y, X = np.histogram(measured, density=True, bins=bins)
     return y, X
 
 # generates a weibull distribution (with random lambda, k, and size)
 def generate_random_weibull_distribution(size, bins):
-    lam = np.random.uniform(*parameters['lambda_range'])
-    k = np.random.uniform(*parameters['k_range'])
+    lam = np.random.uniform(*parameters.get('lambda_range', ()))
+    k = np.random.uniform(*parameters.get('k_range', ()))
     measured = lam*(-np.log(np.random.uniform(0, 1, size)))**(1/k)
     y, X = np.histogram(measured, density=True, bins=bins)
     return y, X
 
 # generates a uniform distribution (with random range and size)
 def generate_random_uniform_distribution(size, bins):
-    low = np.random.uniform(*parameters['low_range'])
-    high = np.random.uniform(*parameters['high_range'])
+    low = np.random.uniform(*parameters.get('low_range', ()))
+    high = np.random.uniform(*parameters.get('high_range', ()))
     measured = np.random.uniform(low, high, size)
     y, X = np.histogram(measured, density=True, bins=bins)
     return y, X
